c60172716fbc388f0231a27addb814329fa4660a
Aashirbad Shahi <ashahi6@myseneca.ca>
2024-05-30 19:34:49 -0400

    lab done

---
+++ lab2b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Prompt user for name
+name = input("Name: ")
+
+# Prompt user for age
+age = input("Age: ")
+
+# Print out message
+print("Hi " + name + ", you are " + age + " years old.")
+
+
